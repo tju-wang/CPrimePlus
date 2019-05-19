2d476e084be5eecd3283622d2091af526b54ad05
tju-wang <1350989027@qq.com>
2019-05-19 15:14:46 +0800

    some code for test

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 
+/*
+C PrimerPlus
+Chapter12.5
+100个1-10的随机数  并以降序排列
+
+*/
 void main(void)
 {
-    printf("Wow!");
+    int arr[100] = {0};
+
+    for(int i=0;i<100;i++)  {
+        arr[i] =  (rand()%10);
+        printf("%d  ",arr[i]);
+    }
+   
+
 
 }
+
+
 
