b650b224c3a0bea3aeea70cf7a34b1a9bd4f2df0
tju-wang <1350989027@qq.com>
2019-05-27 22:50:37 +0800

    Q11.11

--- 11_6/main.c
+++ 11_6/main.c
@@ -3,42 +3,28 @@
 
 /*
 C PrimerPlus
-Chapter11--Q2
+定义指针数组  调用各个题目的测试函数
+
+Chapter11--Q6
+test_11_6()
 在替换处  添加对 空格 制表符的替换  替换为\0  表示读取结束
 // *strlen()函数  参数是接受到的数组时  不能正确返回长度
 // *!!很奇怪的问题  检测不到第一个字符。。。
+Chapter11-8
+
 
 */
 
 char * s_gets(char *st,int n);
-char iswithin(char ch,char *str,int len);
+
+
+extern void test_11_6(void);
+extern void test_11_8(void);
 
 int main (int argc,char *argv[])
 {
-    char mchar[80];
-    char charfind[1];
-    char result;
-
-    printf("input string you want:\n");
-    s_gets(mchar,80);
-
-
-    
-    printf("%d\n",strlen(mchar));
-    int mcharlen = strlen(mchar);
-
-    printf("input char you want find:\n");
-    scanf("%c",charfind);
-
-    if(iswithin(charfind[0],mchar,mcharlen))
-    {
-        printf("find the char in this string");
-    }
-    else
-    {
-        printf("the char can't find in this string");
-    }
-    
+    //test_11_6();
+    test_11_8();
     return 0;
 }
 /*自定义字符串读取函数  
@@ -66,20 +52,5 @@ char * s_gets(char *st,int n)
     return ret_val;
 }
 
-char iswithin(char ch,char *str,int len)    // 找出字符串中是否有该字符 有 返回1  无 返回0
-{
-    char ret = 0;
-    int i;
 
-    for (i=0;i<len;i++)
-    {
-        if(str[i]==ch)
-        {
-            ret = 1;
-            break;
-        }
-    }
-
-    return ret;
-}
 
