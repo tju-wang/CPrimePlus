281a63bc412ee53c2d861f43fac98b781ede101e
tju-wang <1350989027@qq.com>
2019-06-07 15:41:20 +0800

    Finish Q13_10

--- Chapter_13/main.c
+++ Chapter_13/main.c
@@ -6,7 +6,12 @@
 
 extern void test_13_2(int argv, char *argc[]);
 extern void test_13_7(int argc, char *argv[]);
-void test_13_8(int argc, char *argv[]);
+extern void test_13_8(int argc, char *argv[]);
+extern void test_13_10(int argc, char *argv[]);
+
+// extern void test_13_12(int argc, char *argv[]);
+
+
 
 /*
 CPrimerPlus  Chaper13
@@ -25,7 +30,8 @@ int main(int argc, char *argv[])
 
     // test_13_2(argc,argv);
     // test_13_7(argc,argv);
-    test_13_8(argc,argv);
+    // test_13_8(argc,argv);
+    test_13_10(argc,argv);
     return 0;
 }
 
